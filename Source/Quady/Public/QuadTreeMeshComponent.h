c373633dc511cb18e48536c50a38daeddd44f9a1
George <dukecg@gmail.com>
2018-12-22 14:47:58 +1100

    fixes

--- Source/Quady/Public/QuadTreeMeshComponent.h
+++ Source/Quady/Public/QuadTreeMeshComponent.h
@@ -2,25 +2,29 @@
 
 #include "CoreMinimal.h"
 
-struct FQuadyVertexRef
-{
-public:
-    public int16 X;
-    public int16 Y;
-    public int16 QuadX;
-    public int16 QuadY;
+#include "QuadTreeMeshComponent.generated.h"
 
-    FQuadyVertexRef(const int16 X, const int16 Y, const int16 QuadX, const int16 QuadY)
-        : X(X), Y(Y),
-        QuadX(QuadX), QuadY(QuadY) { }
+//
+//struct FQuadyVertexRef
+//{
+//public:
+//    public int16 X;
+//    public int16 Y;
+//    public int16 QuadX;
+//    public int16 QuadY;
+//
+//    FQuadyVertexRef(const int16 X, const int16 Y, const int16 QuadX, const int16 QuadY)
+//        : X(X), Y(Y),
+//        QuadX(QuadX), QuadY(QuadY) { }
+//
+//    uint64 MakeKey() const
+//    {
+//        return (uint64)X << 32 | (uint64)Y << 16 | (uint64)QuadX << 8 | (uint64)QuadY;
+//    }
+//};
 
-    uint64 MakeKey() const
-    {
-        return (uint64)X << 32 | (uint64)Y << 16 | (uint64)QuadX << 8 | (uint64)QuadY;
-    }
-};
-
-UCLASS(HideCategories = (Display, Attachment, Physics, Debug, Collision, Movement, Rendering, PrimitiveComponent, Object, Transform, Mobility), ShowCategories = ("Rendering|Material"), MinimalAPI, Within = QuadTreeMeshProxy)
+// , Within = QuadTreeMeshProxy
+UCLASS(HideCategories = (Display, Attachment, Physics, Debug, Collision, Movement, Rendering, PrimitiveComponent, Object, Transform, Mobility), ShowCategories = ("Rendering|Material"), MinimalAPI)
 class UQuadTreeMeshComponent
     : public UPrimitiveComponent
 {
